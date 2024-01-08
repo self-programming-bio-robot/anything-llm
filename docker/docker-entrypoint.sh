3823bb9ac8980b280e1759202c40a9a31670e353
Zhdanov Nikolai <nvzhdanov@alfabank.ru>
2024-01-08 13:54:50 +0400

    Revert "Test creating directory for documents"
    This reverts commit 48375988a480eae6f3f2d1d5d76d7d9938a34672.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-mkdir -p /app/server/storage/documents/custom-documents
 { cd /app/server/ &&\
   npx prisma generate --schema=./prisma/schema.prisma &&\
   npx prisma migrate deploy --schema=./prisma/schema.prisma &&\
